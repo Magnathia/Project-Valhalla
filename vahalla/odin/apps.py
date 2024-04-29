be09878b6b2a67ad0799d6972fcf38389a812a86
Ronald Smith <Magnathia@gmail.com>
2024-04-29 10:44:48 -0700

    Corrected Spelling

--- vahalla/odin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OdinConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'odin'
--- vahalla/tyr/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser
-
-
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'last_login')
-
-
-admin.site.register(CustomUser)
