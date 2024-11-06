ce46dcfd7afe1271fa80e219ae254038c4348432
Ron Smith <mr.ron.c.smith@gmail.com>
2024-11-06 07:52:03 -0800

    fixed spelling and added Pillow to requirements

--- valhalla/valhalla/wsgi.py
+++ valhalla/valhalla/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for vahalla project.
+WSGI config for valhalla project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vahalla.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'valhalla.settings')
 
 application = get_wsgi_application()
